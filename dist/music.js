5eeb755e48bd047f08639a046fb602c58d237668
EstebanLam <estebanlinou@outlook.co>
2018-08-04 19:38:05 -0400

    Site updated: 2018-08-04 19:38:05

---
+++ dist/music.js
@@ -0,0 +1,43 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "All out of love",
+        artist: 'Air supply',
+        url: 'http://k003.kiwi6.com/hotlink/b144pxkl5h/Air_Supply_-_All_out_of_love.mp3',
+        cover: 'https://is5-ssl.mzstatic.com/image/thumb/Features/e8/a6/a5/dj.tgjdikgn.jpg/268x0w.jpg',
+      },
+      {
+        name: 'City of stars',
+        artist: 'la la land',
+        url: 'http://k003.kiwi6.com/hotlink/0pbgyccgme/Ryan_Gosling_Emma_Stone_-_City_Of_Stars_-_From_La_La_Land_Soundtrack.mp3',
+        cover: 'http://elitegen.singtao.ca/wp-content/uploads/elitegen/2017/04/MV5BMzUzNDM2NzM2MV5BMl5BanBnXkFtZTgwNTM3NTg4OTE@._V1_SY1000_SX675_AL_1.jpg',
+      },
+      {
+        name: 'Versace on the floor',
+        artist: 'Madilyn bailey',
+        url: 'http://k003.kiwi6.com/hotlink/5q7l8tjne7/Madilyn_Bailey_-_Versace_on_the_Floor.mp3',
+        cover: 'https://crossoverer.files.wordpress.com/2010/05/e99fb3e4b990.jpg',
+      },
+      {      
+        name: 'Yellow',
+        artist: 'Coolplay',
+        url: 'http://k003.kiwi6.com/hotlink/yzd1t01qg6/Coldplay_-_Yellow.mp3',
+        cover: 'https://crossoverer.files.wordpress.com/2010/05/e99fb3e4b990.jpg',
+      },
+      {      
+        name: 'Your firefly',
+        artist: 'Ben Cocks',
+        url: 'http://k003.kiwi6.com/hotlink/e298k1okka/Ben_Cocks_-_Your_Firefly_Georgia_Power_and_Toyota_advert_.mp3',
+        cover: 'https://crossoverer.files.wordpress.com/2010/05/e99fb3e4b990.jpg',
+      },
+      {
+        name: 'Ahses',
+        artist: 'Celine Dion',
+        url: 'http://k003.kiwi6.com/hotlink/zydaxhc9fj/Celine_Dion_-_Ashes_deadpool_.mp3',
+        cover: 'https://crossoverer.files.wordpress.com/2010/05/e99fb3e4b990.jpg',
+      },
+    ]
+});
